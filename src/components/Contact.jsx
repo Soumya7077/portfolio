16f2c587d84561a326b0ea045b7bedbb92e9c114
Soumya7077 <110816458+Soumya7077@users.noreply.github.com>
2026-01-13 11:54:43 +0530

    change service id

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     try {
       // Replace these with your EmailJS credentials
       const result = await emailjs.sendForm(
-        'SAZQYtW21HaEJQ6xFECz_',      // Replace with your EmailJS Service ID
+        'service_8l6gsy',      // Replace with your EmailJS Service ID
         'template_xvtw4oz',     // Replace with your EmailJS Template ID
         form.current,
         'ePr3nzX9lV8jBaGSd'       // Replace with your EmailJS Public Key
